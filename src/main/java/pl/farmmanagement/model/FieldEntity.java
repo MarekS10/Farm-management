e881c53ced95dbc35d629646d66855fc137fe2ff
Marek Sysa <marek.sysa@op.pl>
2019-09-07 11:44:58 +0200

    Update FieldEntity

--- src/main/java/pl/farmmanagement/model/FieldEntity.java
+++ src/main/java/pl/farmmanagement/model/FieldEntity.java
@@ -2,10 +2,7 @@ package pl.farmmanagement.model;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
@@ -18,7 +15,9 @@ public class FieldEntity {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column(unique = true,nullable = false)
     private String name;
 
+    @Column(nullable = false)
     private double area;
 }
