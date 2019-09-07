5d3b0722a713cd4fd9187566f71e6f4eb43c281c
Dawid Rosa <rosadawid94@gmail.com>
2019-09-07 10:32:01 +0200

    User entity added.

--- src/main/java/pl/farmmanagement/model/UserEntity.java
+++ src/main/java/pl/farmmanagement/model/UserEntity.java
@@ -1,4 +1,35 @@
 package pl.farmmanagement.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  @Column(unique = true, nullable = false, updatable = false)
+  private String userName;
+
+  @Column(nullable = false)
+  private String password;
+
+  @Column(unique = true, nullable = false)
+  private String eMail;
+
+  @Column(nullable = false)
+  private String givenName;
+
+  @Column(nullable = false)
+  private String surname;
 }
