b78238189b43e1695dbea2bad3a115d14341042f
Dawid Rosa <rosadawid94@gmail.com>
2019-09-07 10:04:02 +0200

    Basic project.

---
+++ src/test/java/pl/farmmanagement/FarmManagementApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.farmmanagement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FarmManagementApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
