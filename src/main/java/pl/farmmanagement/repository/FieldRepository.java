9cf3cabd9f55a34e4adbd8cf5015736fe4f614d3
Marek Sysa <marek.sysa@op.pl>
2019-09-07 11:51:05 +0200

    Add FieldRepository

---
+++ src/main/java/pl/farmmanagement/repository/FieldRepository.java
@@ -0,0 +1,7 @@
+package pl.farmmanagement.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.farmmanagement.model.FieldEntity;
+
+public interface FieldRepository extends JpaRepository<FieldEntity,Long> {
+}
