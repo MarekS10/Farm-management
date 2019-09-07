d62e2a7243b928cea07d96db622b5458e764b386
Marek Sysa <marek.sysa@op.pl>
2019-09-07 18:38:56 +0200

    A saveField method added in the FieldController

--- src/main/java/pl/farmmanagement/controller/FieldController.java
+++ src/main/java/pl/farmmanagement/controller/FieldController.java
@@ -1,23 +1,25 @@
 package pl.farmmanagement.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.farmmanagement.model.FieldDTO;
 import pl.farmmanagement.service.FieldService;
 
+import javax.validation.Valid;
+
 @Controller
 @RequestMapping("/user")
+@RequiredArgsConstructor
 public class FieldController {
 
-    private FieldService fieldService;
-
-    @Autowired
-    public FieldController(FieldService fieldService) {
-        this.fieldService = fieldService;
-    }
+    private final FieldService fieldService;
 
     @GetMapping("/newField")
     public String showFormForAddField(Model theModel){
@@ -25,4 +27,16 @@ public class FieldController {
         theModel.addAttribute("newField",newField);
         return "newField-form";
     }
+
+    @PostMapping("/newField")
+    public String saveField(@ModelAttribute("newField") @Valid FieldDTO field,
+                            BindingResult bindingResult){
+        if (bindingResult.hasErrors()){
+            return "newField-form";
+        }else {
+            fieldService.addField(field);
+            return "redirect:/user";
+        }
+    }
+
 }
