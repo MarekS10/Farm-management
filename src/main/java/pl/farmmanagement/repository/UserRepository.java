b0747e899fbd810eed2004e70cb46ce9c0c75659
Dawid Rosa <rosadawid94@gmail.com>
2019-09-07 11:08:10 +0200

    User repository added.

---
+++ src/main/java/pl/farmmanagement/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package pl.farmmanagement.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.farmmanagement.model.UserEntity;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+}
