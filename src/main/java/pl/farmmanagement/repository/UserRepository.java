fb7565b10ed975a103c6fb0bd55fbad45e071d48
Marek Sysa <marek.sysa@op.pl>
2019-09-09 17:06:03 +0200

    New methods added in the UserRepository

--- src/main/java/pl/farmmanagement/repository/UserRepository.java
+++ src/main/java/pl/farmmanagement/repository/UserRepository.java
@@ -1,9 +1,18 @@
 package pl.farmmanagement.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import pl.farmmanagement.model.FieldEntity;
 import pl.farmmanagement.model.UserEntity;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
-    public UserEntity findByUserName(String name);
+    UserEntity findByUserName(String name);
+    Optional<UserEntity> findByUserNameIgnoreCaseAndPassword(String userName, String password);
+
+    @Query("SELECT userFields from UserEntity u where u.id=?1")
+    List<FieldEntity> userFieldsById(Long id);
 }
