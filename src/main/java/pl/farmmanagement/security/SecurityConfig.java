425b12aa0fa97c2f54238fe60f91fa56ec6d180b
Dawid Rosa <rosadawid94@gmail.com>
2019-09-17 00:03:02 +0200

    Loginpage changed

--- src/main/java/pl/farmmanagement/security/SecurityConfig.java
+++ src/main/java/pl/farmmanagement/security/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests()
-        .antMatchers("/login")
+        .antMatchers("/login","/signUp")
         .permitAll()
         .and()
         .authorizeRequests()
