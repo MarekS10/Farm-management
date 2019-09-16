4749b09d846faa32621791bba1f46e28fbb79b8c
Dawid Rosa <rosadawid94@gmail.com>
2019-09-16 23:40:08 +0200

    Spring security has been changed.

--- src/main/java/pl/farmmanagement/controller/UserController.java
+++ src/main/java/pl/farmmanagement/controller/UserController.java
@@ -7,10 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import pl.farmmanagement.model.FieldEntity;
 import pl.farmmanagement.model.User;
@@ -42,12 +39,10 @@ public class UserController {
         return modelAndView;
 
     }
-
-
-    @GetMapping(value = {"/", "/home"})
-    public ModelAndView showHome(Model model) {
-        ModelAndView modelAndView = new ModelAndView("home.html");
-        model.addAttribute("user",new User());
+    @GetMapping("/login")
+    public ModelAndView loginPage(@RequestParam(required = false) String error) {
+        ModelAndView modelAndView = new ModelAndView("loginPage.html");
+        modelAndView.addObject("error", error);
         return modelAndView;
     }
 
--- src/main/java/pl/farmmanagement/security/SecurityConfig.java
+++ src/main/java/pl/farmmanagement/security/SecurityConfig.java
@@ -10,48 +10,72 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private SecurityUserDetailsService userDetailsService;
+  @Autowired private SecurityUserDetailsService userDetailsService;
+  private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
 
-    @Bean
-    @Primary
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+  @Bean
+  @Primary
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 
-    @Bean
-    public DaoAuthenticationProvider daoAuthenticationProvider() {
-        DaoAuthenticationProvider daoAuthenticationProvider =
-                new DaoAuthenticationProvider();
-        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
-        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
-        return daoAuthenticationProvider;
-    }
+  @Bean
+  public DaoAuthenticationProvider daoAuthenticationProvider() {
+    DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+    daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+    daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+    return daoAuthenticationProvider;
+  }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(daoAuthenticationProvider());
-    }
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) {
+    auth.authenticationProvider(daoAuthenticationProvider());
+  }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/home", "/", "/h2/*", "/h2").permitAll()
-                .and()
-                .formLogin()
-                .loginProcessingUrl("/authenticate")
-                .and()
-                .logout()
-                .logoutUrl("/logout")
-                .invalidateHttpSession(true)
-                .deleteCookies("JSESSIONID")
-                .and().csrf().disable();
-    }
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests()
+        .antMatchers("/login")
+        .permitAll()
+        .and()
+        .authorizeRequests()
+        .antMatchers("/**")
+        .authenticated()
+        .and()
+        .formLogin()
+        .loginPage("/login")
+        .loginProcessingUrl("/auth")
+        //        .successForwardUrl("/user")
+        .successHandler(
+            new AuthenticationSuccessHandler() {
+              @Override
+              public void onAuthenticationSuccess(
+                  HttpServletRequest httpServletRequest,
+                  HttpServletResponse httpServletResponse,
+                  Authentication authentication)
+                  throws IOException, ServletException {
+                redirectStrategy.sendRedirect(httpServletRequest, httpServletResponse, "/user");
+              }
+            })
+        .and()
+        .csrf()
+        .disable();
+  }
 }
